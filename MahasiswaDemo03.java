b6b05ecaf80bf30e41955ec88a1b1ddcd995d130
akmalfitrahari-sib <akmal.fitrahari@gmail.com>
2026-04-10 09:22:27 +0700

    MahasiswaDemo sequential search

---
+++ MahasiswaDemo03.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class MahasiswaDemo03 {
+    public static void main(String[] args) {
+
+        MahasiswaBerprestasi03 list = new MahasiswaBerprestasi03();
+        Scanner sc = new Scanner (System.in);
+        int jumMhs = 5;
+        
+        for (int i = 0; i < jumMhs; i++) {
+            System.out.println("Masukkan Data Mahasiswa ke-" + (i+1));
+            System.out.print("NIM: ");
+            String nim = sc.next();
+            System.out.print("Nama: ");
+            String nama = sc.next();
+            System.out.print("Kelas: ");
+            String kelas = sc.next();
+            System.out.print("IPK: ");
+            double ipk = sc.nextDouble();
+            System.out.println("--------------------"); 
+
+            Mahasiswa03 m = new Mahasiswa03(nim, nama, kelas, ipk);
+            list.tambah(m);
+        }
+        
+        list.tampil();
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("Pencarian data");
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("Masukkan ipk mahasiswa yang dicari: ");
+        System.out.print("IPK: ");
+        double cari = sc.nextDouble();
+
+        System.out.println("Menggunakan sequential searching");
+        double posisi = list.sequentialSearching(cari);
+        int pss = (int) posisi;
+        list.tampilPosisi(cari, pss);
+        list.tampilDataSearch(cari, pss);
+    }
+}
