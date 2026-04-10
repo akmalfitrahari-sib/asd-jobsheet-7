7696c04d2454555a6f2ea5f46fcde2976b389eeb
akmalfitrahari-sib <akmal.fitrahari@gmail.com>
2026-04-10 10:11:38 +0700

    MahasiswaDemo dinamis binary search

--- MahasiswaDemo03.java
+++ MahasiswaDemo03.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
 public class MahasiswaDemo03 {
     public static void main(String[] args) {
-
-        MahasiswaBerprestasi03 list = new MahasiswaBerprestasi03();
         Scanner sc = new Scanner (System.in);
-        int jumMhs = 5;
+
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumMhs = sc.nextInt();
+
+        MahasiswaBerprestasi03 list = new MahasiswaBerprestasi03(jumMhs);
         
         for (int i = 0; i < jumMhs; i++) {
             System.out.println("Masukkan Data Mahasiswa ke-" + (i+1));
@@ -24,7 +26,7 @@ public class MahasiswaDemo03 {
         
         list.selectionSort();
         list.tampil();
-        
+
         System.out.println("-----------------------------------------------------------");
         System.out.println("Pencarian data");
         System.out.println("-----------------------------------------------------------");
@@ -32,9 +34,9 @@ public class MahasiswaDemo03 {
         System.out.print("IPK: ");
         double cari = sc.nextDouble();
 
-        System.out.println("--------------------");
+        System.out.println("-----------------------------------------------------------");
         System.out.println("Menggunakan binary search");
-         System.out.println("--------------------");
+         System.out.println("-----------------------------------------------------------");
          double posisi2 = list.findBinarySearch(cari, 0, jumMhs-1);
         int pss2 = (int)posisi2;
         list.tampilPosisi(cari, pss2);
